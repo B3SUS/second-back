3fc8db023a5ed46d33310841fbdc5d631f2aacf5
B3SUS <IB3SUSI@gmail.com>
2025-01-25 23:25:05 +0300

    v1

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { rateLimit } from 'express-rate-limit'
+import { rateLimit } from 'express-rate-limit';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 10,
+  windowMs: 60 * 1000, // 1 minute
+  max: 10, // Limit each IP to 10 requests per windowMs
   message: "Слишком много запросов с этого IP, попробуйте снова позже.",
 });
 
+// Apply rate limiting middleware to the Express server
+server.use(limiter);
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
     origin: ['http://crypta.space'],
@@ -17,8 +24,13 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.use(limiter)
-
-  await app.listen(3001);
+  await app.init(); // Initialize the NestJS application
 }
-bootstrap();
+
+// Export the serverless handler
+export const handler = async (req: any, res: any) => {
+  if (!server.listening) {
+    await bootstrap(); // Bootstrap the app once for serverless
+  }
+  server(req, res); // Delegate the request to the Express server
+};
